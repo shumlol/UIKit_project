428403804122a56293e24b02343966dcefc8f58e
Vadim Akhma <shummmmmm@gmail.com>
2023-04-17 19:52:50 +0600

    new project

--- UIKit_project/ViewController.swift
+++ UIKit_project/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var segmentControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        segmentControl.insertSegment(withTitle: "Four", at: 3, animated: false)
     }
-
-
+    
+    @IBAction func segmentControlAction() {
+    }
+    
+    private func setupMainLabel() {
+        mainLabel.text = "Quick help text and Label"
+        mainLabel.font = mainLabel.font.withSize(35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+    }
+    
 }
-
