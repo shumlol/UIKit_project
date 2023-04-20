c868b9ec2d92bdf8ed98d706d8704a2ebb73a64d
Vadim Akhma <shummmmmm@gmail.com>
2023-04-20 20:43:12 +0600

    add new element

--- UIKit_project/ViewController.swift
+++ UIKit_project/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet var mainLabel: UILabel!
     @IBOutlet var segmentControl: UISegmentedControl!
     @IBOutlet var slider: UISlider!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var dataPicker: UIDatePicker!
+    @IBOutlet var switchLabel: UILabel!
     
-
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,8 +45,37 @@ class ViewController: UIViewController {
     }
     
     @IBAction func sliderAction() {
+        mainLabel.text = String(slider.value)
+        //view.alpha = CGFloat(slider.value)
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(CGFloat(slider.value)) //UIColor
     }
     
+    @IBAction func doneButtonPressed() {
+        guard let inputText = textField.text, !inputText.isEmpty else {
+            showAlert(with: "Text field is empty", and: "Please enter your name")
+            return
+        }
+        
+        if let _ = Double(inputText) {
+            showAlert(with: "Wrong fornat", and: "Please enter your name")
+            return
+        }
+        mainLabel.text = textField.text
+    }
+    
+    @IBAction func dataPickerAction() {
+        let dataFormatter = DateFormatter()
+        dataFormatter.dateStyle = .long
+        mainLabel.text = dataFormatter.string(from: dataPicker.date)
+    }
+    
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+        dataPicker.isHidden = !sender.isOn
+        switchLabel.text = sender.isOn ? "Hide Data Picker" : "Show Data Picker"
+    }
+    
+
     // MARK: - Private Methods
     private func setupMainLabel() {
         mainLabel.text = String(slider.value)
@@ -54,12 +86,27 @@ class ViewController: UIViewController {
     
     private func setupSlider() {
         slider.value = 1
-        slider.minimumValue = 1
-        slider.maximumValue = 100
+        slider.minimumValue = 0
+        slider.maximumValue = 1
         slider.minimumTrackTintColor = .white
         slider.maximumTrackTintColor = .black
         slider.thumbTintColor = .white
         
     }
     
+}
+
+    // MARK: - UIAlertController
+extension ViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.textField.text = ""
+        }
+        
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
 }
